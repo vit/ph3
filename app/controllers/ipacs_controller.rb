a15cc828fab5dd085fb53d1c27a85aaa43b1b7ba
vit <none@none>
2011-08-30 21:21:46 +0000

    m

--- app/controllers/ipacs_controller.rb
+++ app/controllers/ipacs_controller.rb
@@ -1,5 +1,16 @@
 class IpacsController < ApplicationController
 	def index
+		@news_list = Physcon::App.model.pg.query_inject [], "SELECT newsid, title, content, addtime FROM newsschema.news ORDER BY addtime DESC limit 2" do |acc, row|
+			acc << row
+			acc
+		end
+		@open_conf_list = Physcon::App.model.pg.query_inject [], "SELECT * FROM context WHERE status>100 and cont_type=1 ORDER BY contid DESC" do |acc, row|
+			acc << row
+			acc
+		end
+		#$ctitle1 = htmlspecialchars($row[title]);
+		#$chomepage1 = htmlspecialchars($row[homepage]);
+		#$cdescr1 = nl2br( htmlspecialchars($row[description]) );
 	end
 	def news
 		@data = Physcon::App.model.pg.query_inject [], "SELECT newsid, title, content, addtime FROM newsschema.news ORDER BY addtime DESC" do |acc, row|
