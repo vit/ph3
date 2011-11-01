3f9e25e3ea0a0c26158c50ea8c9485d92493ab05
vit <none@none>
2011-11-01 22:36:14 +0000

    m

--- app/controllers/lib_controller.rb
+++ app/controllers/lib_controller.rb
@@ -9,6 +9,7 @@ class LibController < ApplicationController
 		id = params['id'] && params['id'].to_s.length>0 ? params['id'] : nil
 		@doc = Physcon::App.model.lib.get_doc_info id
 		@children = Physcon::App.model.lib.get_doc_children id
+		@authors = Physcon::App.model.lib.get_doc_authors id
 		@ancestors = Physcon::App.model.lib.get_doc_ancestors id
 		@file_id = Physcon::App.model.lib.find_doc_file id
 		@ancestors.unshift({'_id' => nil, 'title' => 'Root'})
@@ -17,8 +18,13 @@ class LibController < ApplicationController
 		id = params['id'] && params['id'].to_s.length>0 ? params['id'] : nil
 		@file = Physcon::App.model.lib.get_doc_file id
 
-		#@response.header['Content-Type'] = @file.content_type
+		response.headers['Content-Type'] = @file.content_type
+		response.headers["Content-Disposition"] = "attachment; filename=\"#{@file['original_filename']}\""
+		response.headers['Cache-Control'] = 'no-cache, must-revalidate, post-check=0, pre-check=0'
+		#response.headers['Content-Type'] = 'application/pdf'
+		#response.headers['Content-Type'] = 'application/octet-stream'
 		self.response_body = @file.read
+		#response.headers['Content-Type'] = 'application/pdf'
 		#set_content_type(@file.content_type)
 	#	render :content_type => @file.content_type
 	#	render :content_type => @file.content_type
